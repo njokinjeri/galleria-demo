5877b92e83e18d6182bf0b2c9c7869fb4617a0f3
njoki <njerinjoki22@gmail.com>
2026-02-02 13:03:47 +0300

    add slideshow & lightbox styling

--- src/components/Lightbox.tsx
+++ src/components/Lightbox.tsx
@@ -1,17 +1,41 @@
+import { useEffect } from "react";
 type LightboxProps = {
     image: string;
     onClose: () => void
 }
 
 export default function Lightbox({ image, onClose }: LightboxProps) {
-    return (
-        <section>
-            <button
-                onClick={onClose}
-            > Close
-            </button>
-            <img src={image} alt="Full view" />
-        </section>
-    )
 
+    useEffect(() => {
+    document.body.style.overflow = 'hidden';
+    return () => {
+        document.body.style.overflow = 'auto';
+    };
+}, []);
+
+    return (
+        <div className="fixed inset-0 z-100 flex flex-col items-center justify-start bg-black/90 p-6 md:p-12 animate-in fade-in duration-300">
+            
+            <div className="w-full max-w-5xl flex flex-col items-end">
+                <button 
+                    onClick={onClose}
+                    className="mb-8 text-white text-[14px] font-bold tracking-[0.2em] uppercase hover:opacity-50 transition-opacity"
+                >
+                    Close
+                </button>
+
+                <div className="w-full flex justify-center">
+                    <img 
+                        src={image} 
+                        alt="Gallery" 
+                        className="max-h-[75vh] md:max-h-[85vh] w-auto md:w-3/4 h-auto object-contain shadow-2xl select-none"
+                    />
+                </div>
+            </div>
+            <div 
+                className="absolute inset-0 -z-10 cursor-zoom-out" 
+                onClick={onClose}
+            />
+        </div>
+    );
 }
--- src/components/Slide.tsx
+++ src/components/Slide.tsx
@@ -16,31 +16,64 @@ export default function Slide({ painting, onClose, onNext, onPrev, progress}: Sl
     const [showLightbox, setShowLightbox] = useState(false);
 
     return (
-        <section>
+        <article className="min-h-screen flex flex-col gap-8 relative px-6 pb-40 lg:px-10 lg:pb-24 max-w-7xl mx-auto bg-white">
             <div>
-                <button onClick={onClose}>Back to Gallery</button>
+                <button 
+                    onClick={onClose}
+                    className="text-gray-500 font-bold tracking-widest text-xs uppercase hover:text-black transition-colors cursor-pointer"
+                >Back to Gallery</button>
             </div>
-            <div>
-                <img 
-                    src={painting.images.hero.small}
-                    srcSet={`${painting.images.hero.small} 768w, ${painting.images.hero.large} 1280w`}
-                    alt={painting.name}
-                />
-                <button onClick={() => setShowLightbox(true)}>
-                    <img src={viewImage} alt="view image icon" />                    
-                    VIEW IMAGE
-                </button>
-            </div>
-            <div>
-                <div>
-                    <h1>{painting.name}</h1>
-                    <h2>{painting.artist.name}</h2>
-                </div>
-                <div>
-                    <p>{painting.description}</p>
-                    <a href={painting.source} target="_blank"> Go to Source</a>
-                </div>
-            </div>
+            <section className="pt-4 grid grid-cols-1 lg:grid-cols-12 lg:gap-x-10">
+                <section className="relative lg:col-span-8">
+                    <div className="relative">
+                        <img 
+                            src={painting.images.hero.small}
+                            srcSet={`${painting.images.hero.small} 768w, ${painting.images.hero.large} 1280w`}
+                            alt={painting.name}
+                            className="w-full md:max-w-3/4 h-auto object-cover shadow-sm"
+                        />
+                        <button 
+                            onClick={() => setShowLightbox(true)}
+                            className="absolute top-4 left-4 lg:bottom-4 lg:top-auto bg-black/75 text-white px-4 py-3 flex items-center gap-4 text-xs font-bold tracking-[0.2em] hover:scale-90 uppercase cursor-pointer"
+                        >
+                        <img 
+                            src={viewImage} 
+                            alt="view image icon" 
+                            className="size-4" />                    
+                            VIEW IMAGE
+                        </button>
+                    </div>
+                    <div className="absolute -bottom-20 left-0 bg-white p-10 lg:left-auto lg:-right-35 lg:top-0 lg:bottom-auto w-[90%] md:w-[60%] lg:h-fit shadow-sm lg:shadow-none">
+                        <h1 className="text-2xl md:text-5xl font-bold leading-tight text-black">
+                            {painting.name}
+                        </h1>
+                        <h2 className="text-gray-500 mt-2 md:mt-6 text-sm md:text-lg">
+                            {painting.artist.name}
+                        </h2>
+                    </div>
+                </section>
+                <section className="relative lg:col-span-4 flex flex-col items-center lg:items-start pt-24 lg:pt-0">
+                    <div className="absolute left-0">
+                        <img 
+                            src={painting.artist.image} 
+                            alt={painting.artist.name} 
+                            className="size-24 md:size-32 object-cover" 
+                        />
+                    </div>
+                    <span className="absolute right-0 text-[8rem] md:text-[10rem] text-gray-200/60 font-bold z-10 select-none">{painting.year}</span>
+                    <div className="relative">
+                        <p className="text-gray-500 text-base/8 font-extrabold relative z-10 pt-35 px-4 lg:px-0"
+                        >{painting.description}</p>
+                        <a 
+                            href={painting.source} 
+                            target="_blank"
+                            rel="no-referrer"
+                            className="inline-block mt-10 ml-4 lg:ml-0 text-xs font-bold text-gray-500 underline tracking-[0.2em] hover:text-black transition-colors uppercase"
+                            > 
+                            Go to Source</a>
+                    </div>
+                </section>
+            </section>
             <SlideshowControl 
                 name = {painting.name}
                 artist = {painting.artist.name} 
@@ -51,6 +84,6 @@ export default function Slide({ painting, onClose, onNext, onPrev, progress}: Sl
             { showLightbox && (
                 <Lightbox image={painting.images.gallery} onClose={() => setShowLightbox(false)} />
             )}
-        </section>
+        </article>
     )
 }
