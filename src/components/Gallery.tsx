a39cf045db510aa6a4b123682500b6133ab2623f
njoki <njerinjoki22@gmail.com>
2026-02-02 08:19:39 +0300

    add styling to gallery & header

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -7,20 +7,22 @@ type GalleryProps = {
 
 export default function Gallery ({ data, onSelect }: GalleryProps) {
     return (
-        <main>
-            <section>
+        <main className="w-full p-6 md:p-10">
+            <section className="columns-1 sm:columns-2 lg:columns-3 xl:columns-4 gap-10 space-y-10">
                 {data.map((painting, index) => (
                     <div
                         key={index}
                         onClick={() => onSelect(index)}
+                        className="relative break-inside-avoid cursor-pointer group overflow-hidden bg-black"
                     >
                         <img 
                             src={painting.images.thumbnail} 
-                            alt={painting.name} 
+                            alt={painting.name}
+                            className="w-full h-auto block transition-transform duration-500 ease-in-out group-hover:scale-110 opacity-100 group-hover:opacity-80" 
                         />
-                        <div>
-                            <h2>{painting.name}</h2>
-                            <p>{painting.artist.name}</p>
+                        <div className="absolute bottom-0 left-0 w-full p-8 bg-linear-to-t from-black/80 to-transparent text-white">
+                            <h2 className="font-bold text-lg">{painting.name}</h2>
+                            <p className="text-sm opacity-70">{painting.artist.name}</p>
                         </div>
                     </div>
                 ))}
