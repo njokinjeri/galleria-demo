3b270f929545cefdf9330d83ce8d830a56b31030
njoki <njerinjoki22@gmail.com>
2026-02-03 09:50:25 +0300

    fix: allow page scrolling by removing overflow lock

--- src/components/Lightbox.tsx
+++ src/components/Lightbox.tsx
@@ -1,19 +1,9 @@
-import { useEffect } from "react";
-
 type LightboxProps = {
     image: string;
     onClose: () => void
 }
 
 export default function Lightbox({ image, onClose }: LightboxProps) {
-
-    useEffect(() => {
-        document.body.style.overflow = 'hidden';
-        return () => {
-            document.body.style.overflow = 'auto';
-        };
-    }, []);
-
     return (
         <div className="fixed inset-0 z-100 flex flex-col items-center justify-start bg-black/90 p-6 md:p-12 animate-in fade-in duration-300">
             
